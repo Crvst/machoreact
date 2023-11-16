4761a6c0f05c3749f71b0346cd150249b8aeebd2
Isaacfv2404 <jose.fallasvalenciano@ucr.ac.cr>
2023-11-15 23:32:08 -0600

    Update DeleteProduct.js
    se agrego un dato al deleteproduct

--- src/producto/DeleteProduct.js
+++ src/producto/DeleteProduct.js
@@ -3,7 +3,7 @@ import deleteAlert from '../alerts/deleteAlert';
 function DeleteProduct(id,updateProductList) {
 
   const url = `https://localhost:7070/api/Products/${id}`;
-  deleteAlert(url,updateProductList);
+  deleteAlert(url,updateProductList,'el producto');
   
 }
 
