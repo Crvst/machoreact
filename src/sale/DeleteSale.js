33e24fcc14dbbf1a7793b2e58e314e66dd84c7da
Isaacfv2404 <jose.fallasvalenciano@ucr.ac.cr>
2023-11-15 23:39:53 -0600

    Update DeleteSale.js
    se cambio la comilla

--- src/sale/DeleteSale.js
+++ src/sale/DeleteSale.js
@@ -1,7 +1,7 @@
 
 import deleteAlert from "../alerts/deleteAlert";
 const DeleteSale = (id, updateSaleList) => {
-  const url = 'https://localhost:7070/api/Sales/${id}';
+  const url = `https://localhost:7070/api/Sales/${id}`;
   deleteAlert(url, updateSaleList, "la venta");
   
 };
